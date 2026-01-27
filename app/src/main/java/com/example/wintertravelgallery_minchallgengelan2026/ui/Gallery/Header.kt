63e2988976d1bbe5d7e10f7ad80fc5a33820ecf6
slim <helaouislimdev@gmail.com>
2026-01-27 12:58:53 +0100

    [UI] - Add font

--- app/src/main/java/com/example/wintertravelgallery_minchallgengelan2026/ui/Gallery/Header.kt
+++ app/src/main/java/com/example/wintertravelgallery_minchallgengelan2026/ui/Gallery/Header.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,11 +21,10 @@ fun GalleryHeader(
 ) {
     Text(
         text = title.uppercase(),
-        fontWeight = FontWeight.Bold,
         fontSize = 25.sp,
         maxLines = 3,
         overflow = TextOverflow.Ellipsis,
-        style = MaterialTheme.typography.titleMedium,
+        style = MaterialTheme.typography.titleLarge,
         modifier = modifier
             .fillMaxWidth()
             .padding(start = 20.dp, top = 40.dp, bottom = 10.dp),
--- app/src/main/java/com/example/wintertravelgallery_minchallgengelan2026/ui/theme/Type.kt
+++ app/src/main/java/com/example/wintertravelgallery_minchallgengelan2026/ui/theme/Type.kt
@@ -2,33 +2,33 @@ package com.example.wintertravelgallery_minchallgengelan2026.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.example.wintertravelgallery_minchallgengelan2026.R
 
 // Set of Material typography styles to start with
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plusjakartasans_regular, FontWeight.Normal),
+    Font(R.font.plusjakartasans_medium, FontWeight.Medium),
+    Font(R.font.plusjakartasans_bold, FontWeight.Bold),
+    Font(R.font.plusjakartasans_italic, FontWeight.Medium, style = FontStyle.Italic)
+)
+
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Normal
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Bold
+    ),
+    labelLarge = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Medium
     )
-    */
 )
